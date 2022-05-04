663fabae1aaf0373fe48102a20b51e77171e3d3c
ParkChaerim <qkrdbsk28@naver.com>
2022-05-04 23:27:23 +0900

    [CHORE] 카테고리 생성 페이지 수정

--- djangolink/myPage/forms.py
+++ djangolink/myPage/forms.py
@@ -6,8 +6,8 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ['category_name','description']
         widgets = {
-            'category_name': forms.TextInput(attrs={'required': 'true', 'class': 'form-control', 'name':'category_name'}),
-            'description': forms.TextInput(attrs={'required':'true', 'class': 'form-control','name': 'description'}),
+            'category_name': forms.TextInput(attrs={'required': 'true', 'class': 'form-control', 'name':'category_name', 'placeholder':'Category name'}),
+            'description': forms.TextInput(attrs={'required':'true', 'class': 'form-control','name': 'description', 'placeholder':'Category description'}),
         }
         label = {
             'category_name': 'category_name',
